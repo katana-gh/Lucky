eafb4098b39b44ef0f909a5f44cb3decf08db70c
katana-gh <royxalvarez@gmail.com>
2026-05-11 11:29:22 -0500

    turn template

---
+++ turn.py
@@ -0,0 +1,21 @@
+
+"""
+turn begins
+turn ends
+"""
+
+class Turn:
+    def __init__(self):
+        self.turn = 0
+
+    #def is_playable(self, player, card):    
+
+
+    def update_turn(self):
+        self.turn += 1
+
+
+
+
+
+
