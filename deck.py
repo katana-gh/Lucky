277a3d98ade31bf72215ece52a0f1f947215e9e8
katana-gh <royxalvarez@gmail.com>
2026-05-11 11:28:47 -0500

    deck template

---
+++ deck.py
@@ -0,0 +1,54 @@
+
+import random
+from player import Player
+from card import Card
+
+COLORS = ["blue", "green", "red", "yellow"]
+NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+
+
+
+
+    def make_deck(self):
+        card_pair_obj = []
+        for color in COLORS:
+            for num in NUMS:
+                card_pair_obj.append(Card(color, num))
+
+        # turn instance of class into card class
+        for card_obj in card_pair_obj:
+            self.deck.append(card_obj)
+
+        #self.deck += self.deck
+        self.deck
+
+
+    def shuffle(self):
+        length = len(self.deck)
+        shuffle_deck = []
+        for i in range(len(self.deck)):
+            
+            select_random_num = random.randint(0, length - 1)
+            shuffle_deck.append(self.deck[select_random_num])
+            length -= 1
+            del self.deck[select_random_num]
+            
+        self.deck = shuffle_deck
+
+    def draw(self, player_hand):
+        if len(self.deck) != 0:
+            top_card = self.deck.pop()
+            
+
+            player_hand.update_hand(top_card)
+            player_hand.num_in_hand += 1
+            
+            return top_card
+
+    def display_deck_count(self):
+        print(len(self.deck))
+
