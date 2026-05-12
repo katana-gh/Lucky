95c3109685300bfbd8bf6a48346d9815b7eea584
katana-gh <royxalvarez@gmail.com>
2026-05-12 17:58:32 -0500

    add dialogue

---
+++ dialogue.py
@@ -0,0 +1,35 @@
+
+"""
+quotes for Lucky
+"""
+
+
+shredded_from_deck = [
+    "Another day, another card gone missing.",
+    "A card has fallen into the abyss",
+    "The dealer decided to eat a card from the deck",
+    "A card was smuggled out of the deck"
+]
+
+
+
+
+
+
+shredded_from_hand = [
+    "A leprechaun has ran off with your card!",
+    "Does anyone remember the Lucky's lost prevention phone number?",
+    "Nice card, I will take that.",
+    "Did you lose something?",
+    "A card got bored and left.",
+    "Sausage the dog ate  a card.",
+    "Uncle Sam has declared a card tax.",
+    "A card felt claustrophobic and left...",
+    "hand is too full...",
+    "Was the card ever there in the first place?",
+    "A card left for a date.",
+    "A card went to Thailand."
+]
+
+
+
