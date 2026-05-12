623504b5cedad9223d1bfebf201894c5fa1ce767
katana-gh <royxalvarez@gmail.com>
2026-05-12 10:32:26 -0500

    game loop of lucky

---
+++ game_loop.py
@@ -0,0 +1,135 @@
+
+from deck import Deck
+from player import Player
+from turn import Turn
+from card import Card
+from sys import exit
+
+# game actions
+
+def dealing(deck, player, other):
+    counter = 0
+    while counter <= 6:
+        
+        deck.draw(player)
+        player.display_hand()
+
+        counter += 1
+
+    deck.draw(dealer)
+
+
+def play(deck, player, dealer, turn):
+    print("Welcome to Lucky!")
+    print("Play all your cards before the deck reaches 0 to win")
+
+    while True:
+        # special conditions
+        if turn.get_turn() == 0:
+            dealing(deck, player, dealer)
+            
+            turn.update_turn()
+        elif is_victory(player):
+            print("GAME OVER!\n")
+            report(deck, player, dealer, turn)
+            exit()
+        elif is_defeat(deck, player):
+            print("GAME OVER\n")
+            report(deck, player, dealer, turn)
+            exit()
+        
+        dealer_card_obj = dealer.get_hand()[-1]
+        
+        print()
+        deck.display_deck_count()
+        turn.display_turn()
+        display_current_card(dealer)
+        print()
+        player.display_hand()
+
+
+        action = input("Select a card: \ncolor number/draw/quit\n").lower()
+        
+        if action == "draw":
+            draw(deck, player)
+            turn.update_turn()
+            continue
+        elif action == "quit":
+            exit()
+        elif player.is_card_in_hand(action):
+                card_obj = player.played_card(action)
+                
+                if card_obj.is_comparable(dealer_card_obj):
+                    player.remove_card(action)
+                    
+                    dealer.update_hand(card_obj)
+                    
+                    turn.update_turn()
+                    continue
+                else:
+                    print("incorrect card type")
+                    continue
+        else:
+         print("invalid action")
+         continue
+
+
+
+
+
+
+
+
+def draw(deck, player):
+    deck.draw(player)
+
+# game status
+
+def display_current_card(dealer):
+    current_card = dealer.get_hand()[-1].get_card()
+    print(f"Dealer: {current_card}")
+
+
+
+def uno(player):
+    pass
+
+
+def is_victory(player):
+    if player.get_hand_count() == 0:
+        return True
+    
+    return False
+
+def is_defeat(deck, player):
+    if deck.get_deck_count() == 0 and player.get_hand_count() > 0:
+        return True
+    return False
+
+def report(deck, player, dealer, turn):
+    if is_victory(player):
+        outcome = "VICTORY!"
+    else:
+        outcome = "DEFEAT..."
+    print("CREDITS")
+    print(outcome)
+    print(f" You played {dealer.get_hand_count() - 1} cards")
+    print(f"Turns taken {turn.get_turn()}")
+    print(f"Cards in deck remaining {deck.get_deck_count()}")
+    print()
+    print("Created by: Katana")
+
+
+# initialize objects
+
+deck = Deck()
+deck.make_deck()
+
+player = Player()
+
+dealer = Player()
+
+turn_count = Turn()
+
+play_game = play(deck, player, dealer, turn_count)
+
