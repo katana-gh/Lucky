cd57615f14e6ee914c2b559bfec805792c1d0464
katana-gh <royxalvarez@gmail.com>
2026-05-12 10:33:24 -0500

    remove prints

--- card.py
+++ card.py
@@ -1,14 +1,34 @@
 
-COLOR = ["blue", "green", "red", "yellow"]
-NUM = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#from player import Player
+#from deck import Deck
+
 
 class Card:
     def __init__(self, color, num):
         self.color = color
         self.num = num
 
-    def card_color(self):
+    def get_color(self):
         return self.color
 
-    def card_num(self):
+    def get_number(self):
         return self.num
+
+
+    def get_card(self):
+        return f"{self.get_color()} {self.get_number()}"
+
+
+    def is_comparable(self, facing_card_obj):
+        if self.color == facing_card_obj.color:
+            
+            return True
+        if self.num == facing_card_obj.num:
+            
+            return True
+        return False
+
+
+
+
+
